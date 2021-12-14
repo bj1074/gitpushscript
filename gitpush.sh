04ee785711f0d6622e25649da6512b7e5aba2c99
Jayamani <Balaji.Jayamani@commscope.com>
2021-12-14 13:50:52 +0530

    private key backup after the image signer add process

---
+++ gitpush.sh
@@ -0,0 +1,6 @@
+git add .
+git commit -m "private key backup after the image signer add process"
+echo "added the commit"
+git status
+echo "Pushing data to remote server!!!"
+git push -u origin master
